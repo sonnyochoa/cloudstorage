5eadfa608fc38c942086473e6b5193ed7464a353
sonnyochoa <0xsonny@gmail.com>
2021-12-03 15:08:44 -0800

    Building out the foundation for user auth using spring security.

---
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/AuthenticationService.java
@@ -0,0 +1,38 @@
+package com.udacity.jwdnd.course1.cloudstorage.services;
+
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class AuthenticationService implements AuthenticationProvider {
+    private UserMapper userMapper;
+    private HashService hashService;
+
+    public AuthenticationService(UserMapper userMapper, HashService hashService) {
+        this.userMapper = userMapper;
+        this.hashService = hashService;
+    }
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username = authentication.getName();
+        String password = authentication.getCredentials().toString();
+
+        User user = userMapper.getUser(username);
+
+        if (user != null) {
+            String encodedSalt = user.getSalt();
+            String hashedPassword = hashService.getHashedValue(password, encodedSalt);
+
+            if (user.getPassword().equals(hashedPassword)) {
+                System.out.println("=== === == = This is the UsernamePasswordAuthenticationToken = == === ===");
+                return new UsernamePasswordAuthenticationToken(username, password, new ArrayList<>());
+            }
+        }
+    }
+}

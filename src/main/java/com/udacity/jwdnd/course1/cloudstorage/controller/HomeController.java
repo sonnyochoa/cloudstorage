0928bd0a395fcf1457c095308be03298da298578
sonnyochoa <0xsonny@gmail.com>
2021-12-03 18:54:45 -0800

    Created working HomeController with GET

---
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.udacity.jwdnd.course1.cloudstorage.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/home")
+public class HomeController {
+
+    public HomeController() {
+
+    }
+
+    @GetMapping
+    public String getHomePage(Model model) {
+        return "home";
+    }
+}

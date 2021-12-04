9d620596509a68062114e3d1f0bc94eff6abfe6c
sonnyochoa <0xsonny@gmail.com>
2021-12-03 16:00:04 -0800

    Finished implementing SecurityConfig. Fixed a typo with the UserMapper.

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/UserMapper.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/UserMapper.java
@@ -12,7 +12,7 @@ public interface UserMapper {
     User getUser(String user);
 
     @Insert("INSERT INTO USERS (username, salt, password, firstname, lastname) +" +
-            "VALUES(#{username, #{salt}, #{password}, #{firstName}, #{lastName})")
+            "VALUES(#{username}, #{salt}, #{password}, #{firstName}, #{lastName})")
     @Options(useGeneratedKeys = true, keyProperty = "userId")
     int insert(User user);
 }

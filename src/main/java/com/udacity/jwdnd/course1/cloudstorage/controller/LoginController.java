77de283106257582cfe7439425911a4332b2d875
sonnyochoa <0xsonny@gmail.com>
2021-12-03 17:59:57 -0800

    Added more functionality.

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/LoginController.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/LoginController.java
@@ -1,2 +1,15 @@
-package com.udacity.jwdnd.course1.cloudstorage.controller;public class LoginController {
+package com.udacity.jwdnd.course1.cloudstorage.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+
+    @GetMapping()
+    public String loginView() {
+        return "login";
+    }
 }

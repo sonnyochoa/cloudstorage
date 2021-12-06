aa546911596117c9fe6ea52695843394c483deb2
sonnyochoa <0xsonny@gmail.com>
2021-12-06 14:51:23 -0800

    File model created.

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/File.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/File.java
@@ -1,2 +1,68 @@
-package com.udacity.jwdnd.course1.cloudstorage.model;public class File {
+package com.udacity.jwdnd.course1.cloudstorage.model;
+
+public class File {
+    /*
+        fileId INT PRIMARY KEY auto_increment,
+        filename VARCHAR,
+        contenttype VARCHAR,
+        filesize VARCHAR,
+        userid INT,
+        filedata BLOB,
+        foreign key (userid) references USERS(userid)
+     */
+
+    private Integer fieldId;
+    private String fileName;
+    private String contentType;
+    private String fileSize;
+    private Integer userId;
+    private byte[] fileData;
+
+    public void setFieldId(Integer fieldId) {
+        this.fieldId = fieldId;
+    }
+
+    public Integer getFieldId() {
+        return fieldId;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
+
+    public String getContentType() {
+        return contentType;
+    }
+
+    public void setFileSize(String fileSize) {
+        this.fileSize = fileSize;
+    }
+
+    public String getFileSize() {
+        return fileSize;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setFileData(byte[] fileData) {
+        this.fileData = fileData;
+    }
+
+    public byte[] getFileData() {
+        return fileData;
+    }
 }

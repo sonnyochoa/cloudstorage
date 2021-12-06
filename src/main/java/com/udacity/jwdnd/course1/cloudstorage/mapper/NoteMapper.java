5f2b37919a1e2911f928390c20b1b1455e158bf0
sonnyochoa <0xsonny@gmail.com>
2021-12-06 15:27:08 -0800

    Note mapper created.

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/NoteMapper.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/NoteMapper.java
@@ -1,2 +1,18 @@
-package com.udacity.jwdnd.course1.cloudstorage.mapper;public interface NoteMapper {
+package com.udacity.jwdnd.course1.cloudstorage.mapper;
+
+import com.udacity.jwdnd.course1.cloudstorage.model.Note;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface NoteMapper {
+    @Select("SELECT * FROM NOTES WHERE notetitle = #{noteTitle}")
+    Note getNote(String note);
+
+    @Insert("INSERT INTO NOTES (notetitle, notedescription, userid) " +
+            "VALUES (#{noteTitle}, #{noteDescription}, #{userId})")
+    @Options(useGeneratedKeys = true, keyProperty = "noteId")
+    int insert(Note note);
 }
